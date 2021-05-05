e5440365549925fb3fc6d0f17374a6a16769f25e
AnywayMax <anywaymax@gmail.com>
2021-05-05 10:18:29 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace PotokiWork
+{
+    class Program
+    {
+        static void mythread1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Thread 1 call - " + i);
+            }
+        }
+
+        static void mythread2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Thread 2 call - " + i);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(mythread1);
+            Thread thread2 = new Thread(mythread2);
+
+            thread1.Start();
+            thread2.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Thread 3 call - " + i);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
